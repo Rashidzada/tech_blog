a2da682f1553ab86833d7a1b8145d16aebd86055
rashidzada <rashidzada6@gmail.com>
2024-11-26 08:10:04 +0500

    signup functions was added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('login_view/',views.login_view,name='login_view'),
     path('logout_view/',views.logout_view,name='logout_view'),
     path('dashbaord/',views.dashbaord,name='dashbaord'),
+    
+    path('signup/',views.signup,name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
 from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(username = username).exists():
+                messages.warning(request=request,message=f'Username taken')
+            elif User.objects.filter(email = email).exists():
+                messages.warning(request=request,message=f'Email already registered try othethen: {email}')
+            else:
+                User.objects.create_user(username=username,email= email ,password=password , is_staff = True)
+                #  simple user create and then rediret to the login page for login
+                # messages.success(request=request,message=f'You are registred with us thank you:username: {username}: password: {password}')
+                # return redirect('login_view')
+
+                # if we want that user created and at that time also login
+                user = authenticate(username = username, password = password)
+                login(request=request,user=user)
+                messages.success(request=request,message=f'You are registred with us thank you:username: {username}: password: {password}')
+                return redirect('dashbaord')
+        else:
+            messages.warning(request=request,message=f'password not matached')
+    return render(request,'signup.html')
+
+
+
 
 
 def login_view(request):
